12b9006c2336e456f925a59358a77493bcdd68cb
jepariz <je.parizotto@gmail.com>
2022-12-07 09:54:53 -0300

    feat: database config

---
+++ src/server.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Server running in port ${port}`));
