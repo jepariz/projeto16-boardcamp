9fec48897e748fbc5d5710d815138a04e0396834
jepariz <je.parizotto@gmail.com>
2022-12-11 08:29:13 -0300

    fix: verify if the category exists already

--- src/middlewares/categoryValidation.middleware.js
+++ src/middlewares/categoryValidation.middleware.js
@@ -1,16 +1,29 @@
+import { connection } from "../database/database.js";
 import { categorySchema } from "../models/category.model.js";
 
-export function categoryValidation (req, res, next){
+export async function categoryValidation(req, res, next) {
+  const name = req.body;
 
-    const name = req.body;
+  try {
+    const categoryExists = await connection.query(
+      "SELECT * FROM categories WHERE name= $1",
+      [name]
+    );
 
-    const {error} = categorySchema.validate(name, {abortEarly: false})
-
-    if(error){
-        const errors = error.details.map((detail) => detail.message);
-        return res.status(400).send(errors);
+    if (categoryExists) {
+     return res.sendStatus(409);
     }
 
-    res.locals.category = name
-    next()
+    const { error } = categorySchema.validate(name, { abortEarly: false });
+
+    if (error) {
+      const errors = error.details.map((detail) => detail.message);
+      return res.status(400).send(errors);
+    }
+  } catch (err) {
+    res.sendStatus(500);
+  }
+
+  res.locals.category = name;
+  next();
 }
