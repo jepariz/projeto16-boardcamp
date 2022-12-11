c0f0e918da40e75f2cc6b3155b51345fc6c7e47d
jepariz <je.parizotto@gmail.com>
2022-12-11 08:35:36 -0300

    feat: get categories route

--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { categoryValidation } from "../middlewares/categoryValidation.middleware.js";
 import { createCategory } from "../controllers/createCategory.controller.js";
+import { listCategories } from "../controllers/listCategories.controller.js";
 
 const router = Router();
 
-// router.get("/categories",listCategories)
+router.get("/categories",listCategories)
 
 router.post("/categories", categoryValidation, createCategory)
 
