8b83de6e3251580bb25276bf134ee7d30f417cdf
jepariz <je.parizotto@gmail.com>
2022-12-12 15:54:23 -0300

    fix: select rentals query

--- src/controllers/rentalsControllers/listRentals.controller.js
+++ src/controllers/rentalsControllers/listRentals.controller.js
@@ -1,34 +1,55 @@
 import { connection } from "../../database/database.js";
 
+export async function listRentals(req, res) {
+  const customerId = req.query.customerId;
 
-export async function listRentals (req, res){
+  const gameId = req.query.gameId;
 
-    const customerId = req.query.customerId
+  try {
+    if (customerId) {
+      const filteredRentalsByCustomer = await connection.query(
+        `SELECT * FROM rentals WHERE "customerId" ILIKE $1`,
+        [customerId + "%"]
+      );
 
-    const gameId = req.query.gameId
-
-    try{
-        if(customerId){
-            const filteredRentalsByCustomer = await connection.query(
-                `SELECT * FROM rentals WHERE "customerId" ILIKE $1`, [customerId + "%"]
-              );
-
-            return res.send(filteredRentalsByCustomer.rows)
-        }
-
-        if(gameId){
-            const filteredRentalsByGame = await connection.query(
-                `SELECT * FROM rentals WHERE "gameId" ILIKE $1`, [gameId + "%"]
-              );
-
-            return res.send(filteredRentalsByGame.rows)
-        }
-
-        const rentals = await connection.query("SELECT * FROM rentals;")
-        return res.send(rentals.rows)
-
-    }catch (err){
-        res.status(500).send(err.message);
+      return res.send(filteredRentalsByCustomer.rows);
     }
 
+    if (gameId) {
+      const filteredRentalsByGame = await connection.query(
+        `SELECT * FROM rentals WHERE "gameId" ILIKE $1`,
+        [gameId + "%"]
+      );
+
+      return res.send(filteredRentalsByGame.rows);
+    }
+
+    const rentals = await connection.query(`SELECT 
+    json_build_object('id', rentals.id,
+        'customerId', rentals."customerId",
+        'gameId', rentals."gameId",
+        'rentDate', rentals."rentDate",
+        'daysRented', rentals."daysRented",
+        'returnDate', rentals."returnDate",
+        'originalPrice', rentals."originalPrice",
+        'delayFee', rentals."delayFee",
+        'customer', json_build_object(
+            'id', customers.id,
+            'name', customers.name,
+            'game', json_build_object(
+                'id', games.id,
+                'name', games.name,
+                'categoryId', games."categoryId",
+                'categoryName', categories.name 
+               )))
+FROM rentals
+INNER JOIN customers ON rentals."customerId" = customers.id
+INNER JOIN games ON rentals."gameId" = games.id
+INNER JOIN categories ON games."categoryId" = categories.id;
+ `);
+
+    return res.send(rentals.rows);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
